c5f31bf22daad3c8db873469355fe274892fb53b
Carlos Maniero <carlosmaniero@gmail.com>
2016-02-12 17:03:55 -0200

    print mongo url

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ url = os.environ.get('OPENSHIFT_MONGODB_DB_URL')
 host = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
 port = os.environ.get('OPENSHIFT_PYTHON_PORT', 8000)
 
+print(url)
 client = motor.motor_asyncio.AsyncIOMotorClient(url)
 db = client.register_test
